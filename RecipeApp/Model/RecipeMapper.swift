fbdfc092a25cd9b77c3ddbcb82ff729de10c948f
AlAsh88 <shk.ayesha@hotmail.com>
2024-12-10 15:51:46 -0500

    Implement RecipeModel and RecipeMapper - representing structure for data

---
+++ RecipeApp/Model/RecipeMapper.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeMapper.swift
+//  RecipeApp
+//
+//  Created by Ayesha Shaikh on 12/10/24.
+//
+
+import Foundation
+
+struct RecipeMapper: Decodable {
+    let recipes: [RecipeModel]
+    
+}
+
---
+++ RecipeApp/Model/RecipeModel.swift
@@ -0,0 +1,28 @@
+//
+//  RecipeFeed.swift
+//  RecipeApp
+//
+//  Created by Ayesha Shaikh on 12/10/24.
+//
+
+import Foundation
+
+public struct RecipeModel: Codable {
+    public let cuisine: String
+    public let name: String
+    public let photoURLLarge: URL?
+    public let photoURLSmall: URL?
+    public let id: UUID
+    public let sourceURL: URL?
+    public let youtubeURL: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case cuisine
+        case name
+        case photoURLLarge = "photo_url_large"
+        case photoURLSmall = "photo_url_small"
+        case id = "uuid"
+        case sourceURL = "source_url"
+        case youtubeURL = "youtube_url"
+    }
+}
