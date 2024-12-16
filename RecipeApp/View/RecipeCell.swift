41ffa3a71ab3daa6ec53be6c8f91eb5f2886bcf3
AlAsh88 <shk.ayesha@hotmail.com>
2024-12-16 06:38:11 -0500

    implemented table view and cells with IBOutlets with configurations

---
+++ RecipeApp/View/RecipeCell.swift
@@ -0,0 +1,50 @@
+//
+//  RecipeCell.swift
+//  RecipeApp
+//
+//  Created by Ayesha Shaikh on 12/16/24.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+    
+    @IBOutlet weak var cuisineType: UILabel!
+    @IBOutlet weak var recipeImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+//        print(nameLabel == nil ? "$$$$ nameLabel is nil" : "$$$$ nameLabel is connected")
+//        print(cuisineType == nil ? "$$$$ cuisineType is nil" : "$$$$ cuisineType is connected")
+//        print(recipeImageView == nil ? "$$$$ recipeImageView is nil" : "$$$$ recipeImageView is connected")
+        
+    }
+
+    
+    func configure(with recipe: RecipeModel) {
+        nameLabel?.text = recipe.name
+        cuisineType?.text = recipe.cuisine
+        
+//        print("!!!! nameLabel is \(nameLabel == nil ? "nil" : "connected")")
+//        print("!!!! cuisineType is \(cuisineType == nil ? "nil" : "connected")")
+
+        
+        recipeImageView?.image = nil
+        
+        if let photoURL = recipe.photoURLSmall {
+            Task {
+                do {
+                    let image = try await ImageCache.shared.loadImage(from: photoURL)
+                    await MainActor.run {
+                        self.recipeImageView.image = image
+                    }
+                } catch {
+                    print("Failed to load image: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+}
--- RecipeApp/View/RecipeViewController.swift
+++ RecipeApp/View/RecipeViewController.swift
@@ -7,15 +7,51 @@
 
 import UIKit
 
-class RecipeViewController: UIViewController {
+class RecipeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var recipeTableView: UITableView!
+    
+    var recipes: [RecipeModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        recipeTableView.dataSource = self
+        recipeTableView.delegate = self
+        Task {
+            await fetchRecipes()
+        }
         
-        view.backgroundColor = .systemBlue
-    }
+//        print(recipeTableView == nil ? "!!!! TableView is nil" : "!!!! TableView is connected")
 
+    }
+    
+    func fetchRecipes() async {
+        do {
+            recipes = try await RecipeAPIService.shared.fetchRecipes()
+            await MainActor.run {
+                recipeTableView.reloadData()
+            }
+        } catch {
+            // Handle error (e.g., show an alert)
+            print("Error fetching recipes: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        recipes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        print("!!!! Dequeueing cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath) as! RecipeCell
+        let recipe = recipes[indexPath.row]
+        cell.configure(with: recipe)
+//        print(type(of: cell)) // Should print "RecipeCell"
+
+        
+        return cell
+    }
 
 }
 
